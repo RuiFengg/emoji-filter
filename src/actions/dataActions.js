04a4f04965c004056b08b4102a7763880c0c96fe
RuiFengg <ruifeng.chang98@hotmail.com>
2020-06-04 22:26:27 +0800

    for deploymnet

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,18 +1,20 @@
-import { FETCH, FILTER } from './types'
-import axios from 'axios'
+import { FETCH, FILTER } from "./types";
+import axios from "axios";
 
-export const fetchData = () => dispatch =>
-     axios.get(`https://emoji-api.com/emojis?access_key=123412341234`)
-    .then(response => {
-        dispatch({
-            type: FETCH,
-            payload: response.data
-        })
-})
+export const fetchData = () => (dispatch) =>
+  axios
+    .get(
+      `https://emoji-api.com/emojis?access_key=dc6484d1965f280669e79b6acbd037f5cf32e51f`
+    )
+    .then((response) => {
+      dispatch({
+        type: FETCH,
+        payload: response.data,
+      });
+    });
 
-export const filterSearch = event => dispatch =>
-    dispatch({
-        type: FILTER,
-        payload: event
-    })
-    
+export const filterSearch = (event) => (dispatch) =>
+  dispatch({
+    type: FILTER,
+    payload: event,
+  });
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import dataReducer from './reducers/dataReducer';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import dataReducer from "./reducers/dataReducer";
+import { Provider } from "react-redux";
 
-const middleware = [thunk]
+const middleware = [thunk];
 const myStore = createStore(
-  dataReducer, 
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
-  )
-)
+  dataReducer,
+  compose(applyMiddleware(...middleware))
+);
 
 ReactDOM.render(
   <Provider store={myStore}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
